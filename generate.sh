14357f18018470f43707ae79f6dc57168eb7721d
Sencer Yazıcı <senceryazici@gmail.com>
2022-07-14 20:07:45 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-protoc -I=proto --python_out=python --c_out=c --cpp_out=cpp --doc_out=docs proto/acq_protocol.proto
+protoc -I=proto --python_out=python --c_out=c --cpp_out=cpp --doc_out=docs proto/protocol.proto
 
-nanopb_generator proto/acq_protocol.proto --output-dir=nanopb
+nanopb_generator proto/protocol.proto --output-dir=nanopb
